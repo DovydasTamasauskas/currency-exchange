36c380ed0d1e5611e4ab67aa9e62cdf5940ff217
DovydasT <tamasauskas.dovydas@gmail.com>
2024-05-30 12:35:08 +0300

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { fetchData } from "./fetch";
 import CurrencySelect from "./components/CurrencySelect";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import "./App.css";
 
 const App = () => {
@@ -19,13 +21,13 @@ const App = () => {
     }, `exchange?baseCurrency=${baseCurrency}&quoteCurrency=${quoteCurrency}&baseAmount=${amount}`);
   }, [amount, baseCurrency, quoteCurrency]);
 
-  console.log(rates, supportedCurrencies, baseCurrency, quoteCurrency);
+  console.log(rates, supportedCurrencies, baseCurrency, quoteCurrency, amount);
 
   return (
     <>
-      <div className="container mt-5">
+      <div className="container mt-5 overflow-hidden">
         <h1 className="text-center mb-4">Currency Exchange</h1>
-        <div className="row ">
+        <div className="row gy-3">
           <div className="col-6 d-flex justify-content-center">
             <CurrencySelect
               currencies={supportedCurrencies}
@@ -37,6 +39,29 @@ const App = () => {
               currencies={supportedCurrencies}
               onChange={setQuoteCurrency}
             />
+          </div>
+
+          <div className="col-6 d-flex justify-content-center">
+            <TextField
+              id="outlined-basic"
+              sx={{ width: 300 }}
+              label="Amount"
+              variant="outlined"
+              onChange={(e) =>
+                e?.target?.value && setAmount(Number(e.target.value))
+              }
+            />
+          </div>
+          <div className="col-6 d-flex justify-content-center">
+            <Button
+              variant="contained"
+              sx={{ width: 300 }}
+              onClick={() => {
+                console.log("submit");
+              }}
+            >
+              Calculate
+            </Button>
           </div>
         </div>
       </div>
